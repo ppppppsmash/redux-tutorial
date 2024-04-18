714eca9bc03e94ba5afb7f31806ccaa34c5ea58a
ppppppsmash <pppppp.work@gmail.com>
2024-04-18 21:44:33 +0900

    feat: redux state value

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+// ↓ counterSlice.reducer
+import counterReducer from "./counterSlice";
+
 // 状態
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    counter: counterReducer
+  },
 })
