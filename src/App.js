66239ff7d46fb8b20e3cbff3e324b9b4fa401f2a
ppppppsmash <pppppp.work@gmail.com>
2024-04-18 22:15:20 +0900

    feat: increment、decrement event

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import './App.css';
 
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { decrement, increment } from './redux/counterSlice';
 
 function App() {
   // counter: ./redux/store.js counter: counterReducer
   const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
 
   return (
     <div className="App">
       <h1>count: {count}</h1>
+
+      <button onClick={() => dispatch(increment())}>＋</button>
+      <button onClick={() => dispatch(decrement())}>−</button>
     </div>
   );
 }
